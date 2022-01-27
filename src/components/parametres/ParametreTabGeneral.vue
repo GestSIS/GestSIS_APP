551cf06b77a9f8303ca791b233f9c911b73ee7e8
bastien wermeille <bastien.wermeille@gmail.com>
2022-01-27 15:00:17 +0100

    Format code

--- src/components/parametres/ParametreTabGeneral.vue
+++ src/components/parametres/ParametreTabGeneral.vue
@@ -6,9 +6,7 @@
         <!-- /.card-header -->
         <div class="card-header d-flex justify-content-between">
           <h3 class="card-title">General</h3>
-          <button type="button" class="btn btn-primary" @click="save()">
-            Enregister
-          </button>
+          <button type="button" class="btn btn-primary" @click="save()">Enregister</button>
         </div>
         <div class="card-body">
           <div class="mb-3">
@@ -153,7 +151,7 @@
           </div>
         </div>
       </div>
-    </div> -->
+    </div>-->
   </div>
 </template>
 
