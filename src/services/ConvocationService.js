77b26824795ef12a6386f513229a4de83ca1958c
Bastien Wermeille <bastien.wermeille@gmail.com>
2022-07-12 19:06:05 +0200

    Deactivate footer for convocations

--- src/services/ConvocationService.js
+++ src/services/ConvocationService.js
@@ -2,7 +2,7 @@ import Api from '@/http/Request';
 
 export default {
   downloadConvocations(exerciceComptableId, params) {
-    return Api.print('convocations.pdf').get(
+    return Api.print('convocations.pdf', { "no-footer": true }).get(
       '/convocation/' + exerciceComptableId,
       { params }
     );
