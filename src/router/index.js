2c511ba0e025e6858b43afb45f803a4d6424d675
Bastien Wermeille <bastien.wermeille@gmail.com>
2022-10-20 11:01:41 +0200

    Release config settings

--- src/router/index.js
+++ src/router/index.js
@@ -257,7 +257,7 @@ const router = createRouter({
       path: '/materiel-personnel',
       name: 'materiel-personnel',
       beforeEnter: permissionGuard(permissions.MATERIEL_PERSONNEL.LECTURE),
-      component: () => import('@/pages/PageMaterielPersonnel.vue'),
+      component: () => import('@/pages/PageEnTravaux.vue'),
     },
     {
       path: '/utilisateurs',
