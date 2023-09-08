ad7985e0b8d618f1a127155f3c3aaec67df198bd
Bastien Wermeille <bastien.wermeille@gmail.com>
2023-09-08 08:03:30 +0200

    Fix tri mes controles medicaux

--- src/components/mes_infos/MesControlesMedicaux.vue
+++ src/components/mes_infos/MesControlesMedicaux.vue
@@ -80,7 +80,7 @@ export default {
     ...mapState({
       controlesMedicaux: (state) =>
         state.mesInfos.controlesMedicaux
-          .sort((a, b) => a.date?.localeCompare(b.date))
+          .sort((a, b) => b.validite?.localeCompare(a.validite))
           .map((c) => ({
             ...c,
             type: state.controlesMedicauxType.liste.find(
