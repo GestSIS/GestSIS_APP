2155139fc09439cc937a682e5953a407576efc2c
Bastien Wermeille <bastien.wermeille@gmail.com>
2023-11-02 17:49:31 +0100

    Fix ajout SIS

--- src/components/modal/ModalSis.vue
+++ src/components/modal/ModalSis.vue
@@ -88,6 +88,7 @@ export default {
         nom: '',
         abreviation: '',
         mobile: false,
+        api_key: '',
       },
     };
   },
--- src/services/AdminService.js
+++ src/services/AdminService.js
@@ -2,7 +2,7 @@ import Api from '/src/http/Request';
 
 export default {
   addSis(sis) {
-    return Api.auth().post('/sis/', sis);
+    return Api.auth().post('/sis', sis);
   },
   editSis(sis) {
     return Api.auth().patch('/sis/' + sis.id, sis);
