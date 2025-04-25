4e12a5668399eb69155f81886338865d8a70b0ad
Bastien Wermeille <bastien.wermeille@gmail.com>
2025-04-25 14:59:08 +0000

    Format code

--- src/components/materiel/ListeMaterielPourSapeur.vue
+++ src/components/materiel/ListeMaterielPourSapeur.vue
@@ -53,7 +53,7 @@ const computedData = computed(() =>
       type: indexedTypes.value[a.materiel_type_id]?.designation,
       sapeur: indexedSapeurs.value[a.sapeur_id]?.nom_prenom ?? '',
     }))
-    .sort((a1, a2) => a1.type.localeCompare(a2.type))
+    .sort((a1, a2) => a1.type.localeCompare(a2.type)),
 );
 
 const attribuer = () => {
