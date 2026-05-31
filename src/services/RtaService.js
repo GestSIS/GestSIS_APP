4fc500fa8cc17ed8b37ff481e42446f330d711fa
Bastien Wermeille <bastien.wermeille@gmail.com>
2026-05-31 13:10:11 +0000

    Change api endpoints rta

--- src/services/RtaService.js
+++ src/services/RtaService.js
@@ -13,12 +13,12 @@ export default {
     return Api.api().get('/rta/demandes');
   },
   getReferenceRta() {
-    return Api.api().get('/rta');
+    return Api.api().get('/rta/reference');
   },
   getReferenceGestSis() {
-    return Api.api().get('/rta-gestsis');
+    return Api.api().get('/rta/gestsis');
   },
   updateReference(data) {
-    return Api.api().post('/rta', data);
+    return Api.api().post('/rta/reference', data);
   },
 };
