d0c0ec36c325f263a6a5f826debe3cd63eda906e
Bastien Wermeille <bastien.wermeille@gmail.com>
2026-08-06 13:18:56 +0200

    clean of code

--- src/http/Request.js
+++ src/http/Request.js
@@ -95,61 +95,6 @@ const request = {
       },
     });
 
-    // api.interceptors.request.use(async (req) => {
-    //   // Test if expired
-    //   if (Date.now() < (this._accessTokenValidity || 0) * 1000) {
-    //     return req;
-    //   }
-
-    //   // Expired !
-    //   let response = null;
-
-    //   // Check if a refreshToken request has already been sent
-    //   if (refreshTokenPromise.value != '') {
-    //     refreshTokenCountAwait.value++;
-
-    //     // Await the result of this request
-    //     try {
-    //       return await refreshTokenPromise.value;
-    //     } finally {
-    //       refreshTokenCountAwait.value--;
-    //       if (refreshTokenCountAwait.value == 0) {
-    //         refreshTokenPromise.value = '';
-    //       }
-    //     }
-    //   }
-
-    //   // Send a refresh token request
-    //   try {
-    //     refreshTokenPromise.value = this.auth().post('refresh-token', {
-    //       token: this._refreshToken,
-    //     });
-    //     response = await refreshTokenPromise.value;
-    //   } catch (e) {
-    //     if (e?.status === 401) {
-    //       await authStore.logout())
-    //       // useRouter().push({ name: 'login' })
-    //       return Promise.reject(e);
-    //     }
-    //   } finally {
-    //     if (refreshTokenCountAwait.value == 0) {
-    //       refreshTokenPromise.value = '';
-    //     }
-    //   }
-
-    //   // TODO: Update tokens
-    //   // authStore.setTokens();
-    //   this.setAccessToken();
-
-    //   // Update axios
-    //   if (req.headers?.common) {
-    //     req.headers['Authorization'] = `Bearer ${response.accessToken}`;
-    //   }
-    //   this._refreshToken = response.refreshToken;
-
-    //   return req;
-    // });
-
     api.interceptors.response.use(
       (response) => {
         if (response.data.error !== undefined) {
