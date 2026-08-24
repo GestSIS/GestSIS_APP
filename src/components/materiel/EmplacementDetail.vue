2083deb4a7303d744b4c8f1ccdb5c7670ca372fd
Bastien Wermeille <bastien.wermeille@gmail.com>
2026-08-24 12:11:18 +0200

    Improve ui vehicule

--- src/components/materiel/EmplacementDetail.vue
+++ src/components/materiel/EmplacementDetail.vue
@@ -55,69 +55,77 @@ const editEmplacement = () => {
 </script>
 
 <template>
-  <div class="card mb-2">
-    <div class="card-header d-flex justify-content-between align-items-center">
-      <h5 class="m-0">
-        <font-awesome-icon
-          v-if="emplacement.hangar"
-          v-tooltip.bottom="'Hangar'"
-          :icon="['far', 'house']"
-          class="me-1"
-        />
-        <font-awesome-icon
-          v-if="estVehicule"
-          v-tooltip.bottom="'Véhicule'"
-          :icon="['fas', 'car']"
-          class="me-1"
-        />
-        Description de l'emplacement
-      </h5>
-      <button type="button" class="btn btn-primary btn-sm" @click="editEmplacement">
-        <font-awesome-icon :icon="['far', 'edit']" />
-      </button>
+  <div class="row mb-2">
+    <div :class="estVehicule ? 'col-6' : 'col-12'">
+      <div class="card mb-2 h-100">
+        <div class="card-header d-flex justify-content-between align-items-center">
+          <h5 class="m-0">
+            <font-awesome-icon
+              v-if="emplacement.hangar"
+              v-tooltip.bottom="'Hangar'"
+              :icon="['far', 'house']"
+              class="me-1"
+            />
+            Emplacement
+          </h5>
+          <button type="button" class="btn btn-primary btn-sm" @click="editEmplacement">
+            <font-awesome-icon :icon="['far', 'edit']" />
+          </button>
+        </div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-3">Nom</div>
+            <div class="col-9">{{ emplacement.designation }}</div>
+          </div>
+          <div class="row">
+            <div class="col-3">Etiquettage</div>
+            <div class="col-9">
+              <tag-couleur
+                v-for="etiquette in etiquettage"
+                :key="etiquette.id"
+                :couleur="indexedCouleurs[etiquette.couleur_id]"
+                >{{ etiquette.designation }}</tag-couleur
+              >
+              <font-awesome-icon
+                v-if="!emplacement.est_etiquete"
+                v-tooltip.bottom="{
+                  content:
+                    'Cet emplacement spécifique n\'est pas étiquetté sur le matériel correspondant',
+                }"
+                :icon="['far', 'question-circle']"
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-3">Remarque</div>
+            <div class="col-9">
+              {{ formatEmptyString(emplacement.remarque) }}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="card-body">
-      <div class="row">
-        <div class="col-3">Nom</div>
-        <div class="col-9">{{ emplacement.designation }}</div>
-      </div>
-      <template v-if="estVehicule">
-        <div class="row">
-          <div class="col-3">Immatriculation</div>
-          <div class="col-9">{{ formatEmptyString(emplacement.article.immatriculation) }}</div>
+    <div v-if="estVehicule" class="col-6">
+      <div class="card mb-2 h-100">
+        <div class="card-header">
+          <h5 class="m-0">
+            <font-awesome-icon v-tooltip.bottom="'Véhicule'" :icon="['fas', 'car']" class="me-1" />
+            Véhicule
+          </h5>
         </div>
-        <div class="row">
-          <div class="col-3">Chassis</div>
-          <div class="col-9">{{ formatEmptyString(emplacement.article.chassis) }}</div>
-        </div>
-        <div class="row">
-          <div class="col-3">Statut du véhicule</div>
-          <div class="col-9">{{ emplacement.article.statut ? "Actif" : "Inactif" }}</div>
-        </div>
-      </template>
-      <div class="row">
-        <div class="col-3">Etiquettage</div>
-        <div class="col-9">
-          <tag-couleur
-            v-for="etiquette in etiquettage"
-            :key="etiquette.id"
-            :couleur="indexedCouleurs[etiquette.couleur_id]"
-            >{{ etiquette.designation }}</tag-couleur
-          >
-          <font-awesome-icon
-            v-if="!emplacement.est_etiquete"
-            v-tooltip.bottom="{
-              content:
-                'Cet emplacement spécifique n\'est pas étiquetté sur le matériel correspondant',
-            }"
-            :icon="['far', 'question-circle']"
-          />
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-3">Remarque</div>
-        <div class="col-9">
-          {{ formatEmptyString(emplacement.remarque) }}
+        <div class="card-body">
+          <div class="row">
+            <div class="col-3">Immatriculation</div>
+            <div class="col-9">{{ formatEmptyString(emplacement.article.immatriculation) }}</div>
+          </div>
+          <div class="row">
+            <div class="col-3">Chassis</div>
+            <div class="col-9">{{ formatEmptyString(emplacement.article.chassis) }}</div>
+          </div>
+          <div class="row">
+            <div class="col-3">Statut du véhicule</div>
+            <div class="col-9">{{ emplacement.article.statut ? "Actif" : "Inactif" }}</div>
+          </div>
         </div>
       </div>
     </div>
