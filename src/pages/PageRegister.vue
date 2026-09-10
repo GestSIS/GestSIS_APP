fc6e1bf78133a26a2a2f0172c9e8604a9898879d
Bastien Wermeille <bastien.wermeille@gmail.com>
2026-09-10 16:12:00 +0200

    guard no network

--- src/pages/PageRegister.vue
+++ src/pages/PageRegister.vue
@@ -36,7 +36,7 @@ const register = async () => {
       router.push(route.query.redirect ? route.query.redirect : "accueil");
     })
     .catch((data) => {
-      error.value = data.error;
+      error.value = data?.error ?? {};
     })
     .finally(() => {
       submitting.value = false;
